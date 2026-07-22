60fb0eb248e6c7845646695ce9397c7d80fa1e58
Avramradar <avram.aleksey@yandex.com>
2026-07-22 17:09:35 +0300

    Update server.js

--- search-service/server.js
+++ search-service/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import axios from "axios";
 import * as cheerio from "cheerio";
 import pLimit from "p-limit";
+import { searchRussianFood } from "./sources/russianfood.js";
 
 const app = express();
 
